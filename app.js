308387d8061549e4f181e8d7d64003c2923b8c09
huuloc2026 <huuloc2026@gmail.com>
2024-10-12 01:18:47 +0700

    complete weather api

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
+require("dotenv").config();
+const API_KEY = process.env.API_KEY;
 const result = async (location) => {
-  api = `https://api.weatherstack.com/current?access_key=a6200b91e2e0eb885841353f45f4bdbe&query=${location}`;
+  api = `https://api.weatherstack.com/current?access_key=${API_KEY}&query=${location}`;
   try {
     const res = await fetch(api);
     const data = await res.json();
-    if (!res.success) {
-      const weather = {
-        region: data.location.region,
-        country: data.location.country,
-        temperature: data.current.temperature,
-        observation_time: data.current.weather_code,
-        wind_speed: data.current.wind_speed,
-        pressure: data.current.pressure,
-      };
-      console.log(weather);
-    }
+    const weather = {
+      region: data.location.region,
+      country: data.location.country,
+      temperature: data.current.temperature,
+      observation_time: data.current.weather_code,
+      wind_speed: data.current.wind_speed,
+      pressure: data.current.pressure,
+    };
+    return weather;
   } catch (error) {
     console.error(error);
   }
@@ -22,11 +22,20 @@ const result = async (location) => {
 // result("Japan");
 
 const express = require("express");
+const path = require("path");
+const pathPublic = path.join(__dirname, "/public");
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get("/", () => {
-  console.log("Hello World");
+app.set("view engine", "hbs");
+
+app.use(express.static(pathPublic));
+
+app.get("/", async (req, res) => {
+  const city = req.query.city;
+  const data = await result(city);
+  console.log(data);
+  res.render("weather.hbs", { WeatherData: data });
 });
 
 app.listen(port, () => {
